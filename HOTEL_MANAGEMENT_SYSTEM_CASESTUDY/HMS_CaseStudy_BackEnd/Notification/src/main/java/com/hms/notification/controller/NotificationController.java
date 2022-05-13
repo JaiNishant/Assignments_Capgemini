2ed9ca32ef2854db9a300d13e16a8601ef5f28bf
Jai Nishant Gunda <jainishantgunda@gmail.com>
2022-05-13 16:17:30 +0530

    execution project

---
+++ HOTEL_MANAGEMENT_SYSTEM_CASESTUDY/HMS_CaseStudy_BackEnd/Notification/src/main/java/com/hms/notification/controller/NotificationController.java
@@ -0,0 +1,54 @@
+package com.hms.notification.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hms.notification.models.GuestNotification;
+import com.hms.notification.models.ReservationNotification;
+import com.hms.notification.service.GuestMailService;
+import com.hms.notification.service.ReservationMailService;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+@RestController
+@RequestMapping("/Notification")
+public class NotificationController {
+
+	private final static String ACCOUNT_SID = "ACf226aeb779bb44959cca044707a1b67a";
+	private final static String AUTH_ID = "f50100db73a6e5667bcb2408c148ef8a";
+
+	static {
+		Twilio.init(ACCOUNT_SID, AUTH_ID);
+	}
+
+	@Autowired
+	private ReservationMailService reservationMail;
+
+	@Autowired
+	private GuestMailService guestMail;
+
+	// Sends notification(mail & SMS) to the given phone number and mail ID when
+	// reservation completes
+	@PostMapping("/reservationnotification")
+	public void run(@RequestBody ReservationNotification details) throws Exception {
+		Message.creator(new PhoneNumber(details.getPhoneNumber()), new PhoneNumber("+17579934689"),
+				"Dear " + details.getName() + " Thank you for your reservation to BABAI HOTEL . Your reservation code is "
+						+ details.getReservationCode())
+				.create();
+		reservationMail.sendmail(details);
+	}
+
+	// Sends notification(mail & SMS) to the given phone number and mailID when
+	// guest sent to specific room
+	@PostMapping("/guestnotification")
+	public void guestRun(@RequestBody GuestNotification details) throws Exception {
+		Message.creator(new PhoneNumber(details.getPhoneNumber()), new PhoneNumber("+17579934689"),
+				"Dear " + details.getName() + " Your guest code is " + details.getGuestCode()).create();
+		guestMail.sendmail(details);
+	}
+
+}
